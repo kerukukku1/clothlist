48e79eac55adf94d3574f21eb22d825f30c45af7
Mahito Fujino <kerukukku1@gmail.com>
2018-11-06 20:42:30 +0900

    implemets ClothBox and Item

---
+++ src/views/ClothBoxItem.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="cloth-item">
+        <slot></slot>
+    </div>
+</template>
+
+<script lang="ts">
+import {Vue, Component} from 'vue-property-decorator'
+
+@Component({})
+export default class ClothBoxItem extends Vue{
+
+}
+</script>
+
+<style scoped>
+.cloth-item{
+    background-color: white;
+    border-radius: 10px;
+    border-color: black;
+    padding: 20px;
+    height: 200px;
+}
+</style>
