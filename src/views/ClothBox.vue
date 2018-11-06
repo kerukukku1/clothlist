edf689ae9269a15c2edcd37848d96252051b0c2c
Mahito Fujino <kerukukku1@gmail.com>
2018-11-06 20:07:46 +0900

    make ClothBox.vue and write template

---
+++ src/views/ClothBox.vue
@@ -0,0 +1,16 @@
+<template>
+    
+</template>
+
+<script lang="ts">
+import {Vue, Component} from 'vue-property-decorator'
+
+@Component({})
+export default class ClothBox extends Vue{
+
+}
+</script>
+
+<style scoped>
+
+</style>
