b96241cc0ff4e307ee3f80246a7d96872aa22ccf
Mahito Fujino <kerukukku1@gmail.com>
2018-11-11 16:42:23 +0900

    implements file save with golang

--- src/server/connect.go
+++ src/server/connect.go
@@ -4,17 +4,27 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"math/rand"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
-	"gopkg.in/mgo.v2/bson"
 )
 
-type Person struct {
-	ID   bson.ObjectId `bson:"_id"`
-	Name string        `bson:"name"`
-	Age  int           `bson:"age"`
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+var rs1Letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func RandString1(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = rs1Letters[rand.Intn(len(rs1Letters))]
+	}
+	return string(b)
 }
 
 func test(w http.ResponseWriter, r *http.Request) {
@@ -22,25 +32,37 @@ func test(w http.ResponseWriter, r *http.Request) {
 
 func PostImageBlob(w http.ResponseWriter, r *http.Request) {
 	fmt.Print("post detect")
-	reader, err := r.MultipartReader()
+	err := r.ParseMultipartForm(32 << 20) // maxMemory
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	for {
-		tar, err := reader.NextPart()
-		if err == io.EOF {
-			break
-		}
-		if tar.FileName() == "" {
-			continue
-		}
-		fmt.Printf("%+v", tar)
+	fmt.Print("memory ok")
+	file, _, err := r.FormFile("imagefile")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
+	fmt.Print("file get ok")
+	defer file.Close()
+	title := r.FormValue("title")
+	if len(title) == 0 {
+		return
+	}
+	fmt.Print("title get ok")
+	f, err := os.Create("./images/" + RandString1(20) + ".jpg")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	fmt.Print("image save ok")
+	defer f.Close()
+	io.Copy(f, file)
 }
 
 func main() {
 	router := mux.NewRouter()
+	os.Mkdir("images", 0777)
 	allowedOrigins := handlers.AllowedOrigins([]string{"http://localhost:8080"})
 	allowedMethods := handlers.AllowedMethods([]string{"GET", "POST", "DELETE", "PUT", "OPTIONS"})
 	allowedHeaders := handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"})
