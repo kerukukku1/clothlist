ae0b7d94c8160efd8be0709288cc09e6f0297f53
Mahito Fujino <kerukukku1@gmail.com>
2018-11-12 21:22:45 +0900

    imoplements reactive backgroundimage change function

--- src/components/ClothBoxImage.vue
+++ src/components/ClothBoxImage.vue
@@ -1,14 +1,34 @@
 <template>
-    <div class="cloth-box-image">
+    <!-- <div class="cloth-box-image"> -->
+    <div :style="boximage">
     </div>
 </template>
 
 <script lang="ts">
 import {Vue, Component} from 'vue-property-decorator'
 
+interface boxImage{
+    width : string,
+    height : string,
+    minHeight : string,
+    backgroundSize : string,
+    backgroundPosition : string,
+    "background-image" : string
+}
+
 @Component({})
 export default class ClothBoxImage extends Vue{
-
+    boximage: boxImage = {
+        width : "100%",
+        height : "200px",
+        minHeight : "200px",
+        backgroundSize : "cover",
+        backgroundPosition : "center",
+        'background-image' : ''
+    }
+    created() {
+        Vue.set(this.boximage, 'backgroundImage', 'url('+require("../assets/users/mahito6/IMG_7696.jpg")+')')
+    }
 }
 </script>
 
@@ -19,7 +39,7 @@ export default class ClothBoxImage extends Vue{
     min-height: 100%;
     background-size: cover;
     background-position: center;
-    background-image: url("../assets/users/mahito6/IMG_7696.jpg");
+    background-image: url("../assets/users/mahito6/IMG_7696.jpg")
 }
 
 </style>
